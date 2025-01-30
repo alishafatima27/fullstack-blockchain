360ebb4f0cb6b1b210738f4698df7a77540db6cf
Alisha Fatima <aaafiyasab@gmail.com>
2025-01-31 00:58:11 +0530

    feat: Implemnted blockchain core functionality with TDD, Jest and SHA256

---
+++ block.js
@@ -0,0 +1,33 @@
+const { GENESIS_DATA } = require("./config");
+const cryptoHash = require('./crypto-hash');
+
+
+class Block{
+    constructor({timestamp, lastHash, hash, data}){ //{agruments in this do not nessary to be in order it }
+        this.data = data;
+        this.hash = hash;
+        this.lastHash = lastHash;
+        this.timestamp = timestamp;
+    }
+
+
+    static genesis() {
+        return new this(GENESIS_DATA);
+    } 
+
+
+    static mineBlock({ lastBlock, data }) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        
+        return new this({
+            timestamp,
+            lastHash,
+            data,
+            hash: cryptoHash(timestamp, lastHash, data)
+        });
+    }
+}
+
+module.exports = Block;
+
---
+++ config.js
@@ -0,0 +1,8 @@
+const GENESIS_DATA = {// Stream case syntax hardcoded global value 
+    timestamp : 1,
+    lastHash: '-----',
+    hash: 'hash-one',
+    data: []
+}; 
+
+module.exports = {GENESIS_DATA};
